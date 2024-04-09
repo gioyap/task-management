a2e13e95f2dd38d81abf6866ccdbde5b6239c195
Gio Yap <gioedrian.yap.l@gmail.com>
2024-04-09 21:33:29 +0800

    fix the task

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TasksController } from './tasks.controller';
-import { TasksService } from './tasks.service';
-import { TasksRepository } from './tasks.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.entity';
+import { TaskRepository } from './tasks.repository';
+ 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
+  imports: [TypeOrmModule.forFeature([Task])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, TaskRepository],
 })
 export class TasksModule {}
